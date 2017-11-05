966eeb10e665db7eb46a6e4cc3cbf7b385fb4b6d
Jorge Cano <jacano.sosa@gmail.com>
2017-11-04 18:29:53 -0600

    Starting Point

---
+++ app/src/main/java/com/uady/jorge_cano/serviciosatualcance/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.uady.jorge_cano.serviciosatualcance;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view){
+
+        Intent intent = new Intent(this, StartActivity.class);
+        startActivity(intent);
+    }
+
+    public void register(View view){
+
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+}
