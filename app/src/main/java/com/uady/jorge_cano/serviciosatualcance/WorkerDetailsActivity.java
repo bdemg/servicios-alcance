a8820af084c5e61c038c26ef7e47e4670aa1ee0b
Jorge Cano <jacano.sosa@gmail.com>
2017-11-12 17:58:13 -0600

    Creación de la pantalla del trabajador doméstico

---
+++ app/src/main/java/com/uady/jorge_cano/serviciosatualcance/WorkerDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.uady.jorge_cano.serviciosatualcance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkerDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worker_details);
+    }
+}
