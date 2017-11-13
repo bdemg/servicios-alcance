fbae9a30f85e2e8890f4b40f2794c9650f150e79
Jorge Cano <jacano.sosa@gmail.com>
2017-11-13 11:04:43 -0600

    ListAdapter para la lista de comentarios

---
+++ app/src/main/java/com/uady/jorge_cano/serviciosatualcance/model/Comment.java
@@ -0,0 +1,30 @@
+package com.uady.jorge_cano.serviciosatualcance.model;
+
+/**
+ * Created by jorge-cano on 13/11/17.
+ */
+
+public class Comment {
+
+    private String name;
+    private String comment;
+    private float rating;
+
+    public Comment(String name, String comment, double rating) {
+        this.name = name;
+        this.comment = comment;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+}
